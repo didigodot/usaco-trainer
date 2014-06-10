2d4138ef52fef9f76b1d9c02eff408bd6c36749c
Didi Park <cellofortist@gmail.com>
2014-06-10 11:26:27 -0400

    turning stuff in

---
+++ 4beads/beads.java
@@ -0,0 +1,23 @@
+/*
+ID: cellofo1
+PROG: beads
+LANG: JAVA
+*/
+import java.util.*;
+import java.io.*;
+
+public class beads
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader in = new BufferedReader(new FileReader("beads.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("beads.out")));
+        int N = Integer.parseInt(in.readLine());
+        String s = in.readLine();
+        
+
+        out.close();
+        System.exit(0);
+    }
+}
+
